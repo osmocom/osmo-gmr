adb8cc6aa9453dfc339de53ae641067136a031ec
Harald Welte <laforge@gnumonks.org>
2017-11-14 14:15:21 +0900

    Add contrib/jenkins.sh script, like other osmo-* repositories
    This simplifies our jenkins setup and uses shared scripts from
    osmo-ci.git
    
    Change-Id: I025c4ed919bdfa98914f8161d361fb7c92088e95

---
+++ contrib/jenkins.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# jenkins build helper script for openbsc.  This is how we build on jenkins.osmocom.org
+
+if ! [ -x "$(command -v osmo-build-dep.sh)" ]; then
+	echo "Error: We need to have scripts/osmo-deps.sh from http://git.osmocom.org/osmo-ci/ in PATH !"
+	exit 2
+fi
+
+
+set -ex
+
+base="$PWD"
+deps="$base/deps"
+inst="$deps/install"
+export deps inst
+
+osmo-clean-workspace.sh
+
+mkdir "$deps" || true
+
+osmo-build-dep.sh libosmocore "" ac_cv_path_DOXYGEN=false
+
+export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH="$inst/lib"
+
+osmo-build-dep.sh libosmo-dsp
+
+set +x
+echo
+echo
+echo
+echo " =============================== osmo-gmr ==============================="
+echo
+set -x
+
+cd "$base"
+
+autoreconf --install --force
+./configure 
+$MAKE $PARALLEL_MAKE
+LD_LIBRARY_PATH="$inst/lib" $MAKE check \
+  || cat-testlogs.sh
+LD_LIBRARY_PATH="$inst/lib" \
+  $MAKE distcheck \
+  || cat-testlogs.sh
+
+osmo-clean-workspace.sh
